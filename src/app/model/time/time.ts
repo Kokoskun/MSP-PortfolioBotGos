95a960db83d5a5d4173013b4289e2a994230b96a
PATIPAN NAKARIN <Kokoskun@gmail.com>
2017-08-17 09:45:25 +0700

    PorfolioBotGos Version0.1.1
    Update and Edit Angular

--- src/app/model/time/time.ts
+++
@@ -1,4 +0,0 @@
-export class Time {
-	constructor(private dayofweekName:string,private day:number,private month:number,private monthName:string,private year:number,private hours:number,private minutes:number) {
-	}
-}
